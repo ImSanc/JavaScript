11575a16fbfbd372df79a801bf53e33419339d08
Sanchit mishra <sanchit1417@gmail.com>
2024-06-14 18:50:32 +0530

    need to create another directory for this

---
+++ express/fileServer.js
@@ -0,0 +1,58 @@
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+    - For any other route not defined in the server return 404
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs").promises;
+const app = express();
+app.listen(3000);
+const directoryPath = path.join(__dirname, "files");
+
+app.get("/files", async (request,response)=>{
+ 
+  try{
+  const files = await getFiles();
+  response.json(files).status(200);
+  }
+  catch(err)
+  {
+      response.json({Error : "Directory doesnt exist"}).status(403);
+  }
+})
+
+async function getFiles()
+{
+  try{
+    const files = await fs.readdir(directoryPath);
+    const fileStats = await Promise.all(
+      files.map(async file =>{
+        const filePath = path.join(directoryPath,file);
+        const stat = await fs.stat(filePath);
+        return {file, isFile : stat.isFile()};
+      })
+    );
+
+    const fileNames = fileStats.filter((fileStat => fileStat.isFile))
+    .map( fileStat =>fileStat.file);
+    return fileNames;
+  }
+  catch(err)
+  {
+    throw new Error("Failed to read directory");
+  }
+}
+
+
+
