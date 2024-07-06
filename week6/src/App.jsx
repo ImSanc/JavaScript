817de9f346e87dd571a34ba57899147b2643bb58
Sanchit mishra <sanchit1417@gmail.com>
2024-07-06 13:06:28 +0530

    using memo

--- week6/src/App.jsx
+++ week6/src/App.jsx
@@ -1,43 +1,37 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
-
- 
-  return (
-    <>
-     <HeaderWithButton></HeaderWithButton>
-     <Header title = "Sanchit" ></Header>
-     </>
-  )
-}
-
-function HeaderWithButton() {
   const [title, setTitle] = useState("Sanchit")
 
-  function changeHeader() {
-    const random = Math.random();
-    setTitle(random);
-  }
+  function changeHeader()
+{
+  const random = Math.random();
+  setTitle(random);
+}
 
   return (
-    <>
-    <button onClick= { changeHeader} >Click on me to change the title </button>
+    <div>
+     <button onClick= { changeHeader} >Click on me to change the title </button>
      <Header title = {title} ></Header>
-    </>
+     <Header title = "Sanchit" ></Header>
+     <Header title = "Sanchit" ></Header>
+     <Header title = "Sanchit" ></Header>
+     <Header title = "Sanchit" ></Header>
+     <Header title = "Sanchit" ></Header> 
+     </div>
   )
 }
 
 
 
-function Header(props)
+const Header = React.memo(function Header(props)
 {
   return (<div>
     <h1>{props.title}</h1>
   </div>)
-}
+})
 
 export default App
