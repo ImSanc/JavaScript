38f4f65a1da909068bf31c3b20008dbd4b0e8983
Sanchit mishra <sanchit1417@gmail.com>
2024-07-08 15:26:12 +0530

    Create todo app

--- week6/src/App.jsx
+++ week6/src/App.jsx
@@ -3,35 +3,57 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let idCounter = 4;
 function App() {
-  const [title, setTitle] = useState("Sanchit")
+ const [todos,setTodos] = useState([
+  { 
+    id : 1,
+  title : "sanchit",
+  description : "Sanchit needs to work"
+  },
+  {
+    id : 2,
+    title : "DevSan",
+    description : "Sanchit needs to dev"
+  }, {
+    id : 3,
+    title : "chillSan",
+    description : "Sanchit needs to chill"
+  }
 
-  function changeHeader()
+]);
+
+function addTodo ()
 {
-  const random = Math.random();
-  setTitle(random);
+  setTodos([...todos,{
+    id : idCounter++,
+    title : "new todo ",
+    description : "Added new Todo"
+  }])
 }
 
-  return (
-    <div>
-     <button onClick= { changeHeader} >Click on me to change the title </button>
-     <Header title = {title} ></Header>
-     <Header title = "Sanchit" ></Header>
-     <Header title = "Sanchit" ></Header>
-     <Header title = "Sanchit" ></Header>
-     <Header title = "Sanchit" ></Header>
-     <Header title = "Sanchit" ></Header> 
-     </div>
-  )
+return (
+  <div>
+    <button onClick={addTodo}>Add Todo</button>
+  <Todo todos={todos}></Todo>
+  </div>
+);
+
+
 }
 
-
-
-const Header = React.memo(function Header(props)
-{
-  return (<div>
-    <h1>{props.title}</h1>
-  </div>)
-})
+function Todo ({todos}) {
+  return ( <div> 
+  { todos.map( (todo) => {
+    
+    return (<div>
+      key = {todo.id}
+      <h1>{todo.title}</h1>
+      <h1>{todo.description}</h1>
+      </div>
+    )
+  })}
+    </div>)
+}
 
 export default App
