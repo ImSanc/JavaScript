386703f04993f660ceaaa502ec73a0221c0e0d1e
Sanchit mishra <sanchit1417@gmail.com>
2024-07-18 00:34:42 +0530

    trying with window

--- week7/week7.1/src/App.jsx
+++ week7/week7.1/src/App.jsx
@@ -6,12 +6,21 @@ import { Landing } from './components/Landing'
 
 function App() {
     return (
+        <div>
+            <button onClick={ ()=>{
+                window.location.href = "/";
+            }}>Back</button>
+
+            <button onClick={()=>{
+                window.location.href = "/dashboard";
+            }}>forward</button>
         <BrowserRouter>
             <Routes>
                 <Route  path = '/dashboard' element={<Dashboard/>} ></Route>
                 <Route path='/' element={<Landing/>}></Route>
             </Routes>
         </BrowserRouter>
+        </div>
     )
 }
 
