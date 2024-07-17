09e6978629e44c5cd690762b2d107a9369fd4d31
Sanchit mishra <sanchit1417@gmail.com>
2024-07-17 20:35:05 +0530

    Adding routing

--- week7/week7.1/src/App.jsx
+++ week7/week7.1/src/App.jsx
@@ -1,7 +1,18 @@
 
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/landing'
 
 function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route  path = '/dashboard' element={<Dashboard/>} ></Route>
+                <Route path='/landing' element={<Landing/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
