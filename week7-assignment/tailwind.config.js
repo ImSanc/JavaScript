b4278154e5ce59a3b9079a9a54d2312962c1be0b
Sanchit mishra <sanchit1417@gmail.com>
2024-08-14 15:49:55 +0530

    week 7 assignment 1

---
+++ week7-assignment/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors :{
+        seaGreen : {
+          DEFAULT : '#20b2aa'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
